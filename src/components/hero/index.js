2229ce6178329fd71bc05408d65fc2b43d05be06
kimash-255 <rexviscot44@gmail.com>
2025-01-29 21:19:23 +0300

    slider-hero

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -1,158 +1,71 @@
-import Image from "next/image";
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 import Link from "next/link";
 
 const Hero = () => {
+  const slides = [
+    { id: 1, image: "/assets/img/hero/slider-1.jpg" },
+    { id: 2, image: "/assets/img/hero/slider-2.jpg" },
+    { id: 3, image: "/assets/img/hero/slider-3.jpg" },
+  ];
+
   return (
     <div className="hero-section">
-      <div className="hero-slider owl-carousel">
-        <div
-          className="hero-single"
-          style={{
-            backgroundImage: "url(/assets/img/hero/slider-1.jpg)",
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        >
-          <div className="container">
-            <div className="row align-items-center">
-              <div className="col-md-12 col-lg-6">
-                <div className="hero-content">
-                  <h6
-                    className="hero-sub-title"
-                    data-animation="fadeInUp"
-                    data-delay=".25s"
-                  >
-                    <i className="far fa-house-chimney-heart"></i> Best Elderly
-                    Care
-                  </h6>
-                  <h1
-                    className="hero-title"
-                    data-animation="fadeInRight"
-                    data-delay=".50s"
-                  >
-                    Live in care & <span>your family</span> will love
-                  </h1>
-                  <p data-animation="fadeInLeft" data-delay=".75s">
-                    There are many variations of passages orem psum available
-                    but the majority have suffered alteration in some form by
-                    injected humour.
-                  </p>
-                  <div
-                    className="hero-btn"
-                    data-animation="fadeInUp"
-                    data-delay="1s"
-                  >
-                    <Link href="/about" className="theme-btn">
-                      About More<i className="fas fa-arrow-right"></i>
-                    </Link>
-                    <Link href="/contact" className="theme-btn2">
-                      Learn More<i className="fas fa-arrow-right"></i>
-                    </Link>
+      <Swiper
+        modules={[Autoplay, Pagination, Navigation]}
+        spaceBetween={0}
+        slidesPerView={1}
+        autoplay={{ delay: 5000, disableOnInteraction: false }}
+        pagination={{ clickable: true }}
+        navigation={true}
+        loop={true}
+      >
+        {slides.map((slide) => (
+          <SwiperSlide key={slide.id}>
+            <div
+              className="hero-single"
+              style={{
+                backgroundImage: `url(${slide.image})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+            >
+              <div className="container">
+                <div className="row align-items-center">
+                  <div className="col-md-12 col-lg-6">
+                    <div className="hero-content">
+                      <h6 className="hero-sub-title">
+                        <i className="far fa-house-chimney-heart"></i> Best
+                        Elderly Care
+                      </h6>
+                      <h1 className="hero-title">
+                        Live in care & <span>your family</span> will love
+                      </h1>
+                      <p>
+                        There are many variations of passages of Lorem Ipsum,
+                        but the majority have suffered alteration in some form
+                        by injected humour.
+                      </p>
+                      <div className="hero-btn">
+                        <Link href="/about" className="theme-btn">
+                          About More<i className="fas fa-arrow-right"></i>
+                        </Link>
+                        <Link href="/contact" className="theme-btn2">
+                          Learn More<i className="fas fa-arrow-right"></i>
+                        </Link>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-        <div
-          className="hero-single"
-          style={{
-            backgroundImage: "url(/assets/img/hero/slider-2.jpg)",
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        >
-          <div className="container">
-            <div className="row align-items-center">
-              <div className="col-md-12 col-lg-6">
-                <div className="hero-content">
-                  <h6
-                    className="hero-sub-title"
-                    data-animation="fadeInUp"
-                    data-delay=".25s"
-                  >
-                    <i className="far fa-house-chimney-heart"></i> Best Elderly
-                    Care
-                  </h6>
-                  <h1
-                    className="hero-title"
-                    data-animation="fadeInRight"
-                    data-delay=".50s"
-                  >
-                    Live in care & <span>your family</span> will love
-                  </h1>
-                  <p data-animation="fadeInLeft" data-delay=".75s">
-                    There are many variations of passages orem psum available
-                    but the majority have suffered alteration in some form by
-                    injected humour.
-                  </p>
-                  <div
-                    className="hero-btn"
-                    data-animation="fadeInUp"
-                    data-delay="1s"
-                  >
-                    <Link href="/about" className="theme-btn">
-                      About More<i className="fas fa-arrow-right"></i>
-                    </Link>
-                    <Link href="/contact" className="theme-btn2">
-                      Learn More<i className="fas fa-arrow-right"></i>
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div
-          className="hero-single"
-          style={{
-            backgroundImage: "url(/assets/img/hero/slider-3.jpg)",
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        >
-          <div className="container">
-            <div className="row align-items-center">
-              <div className="col-md-12 col-lg-6">
-                <div className="hero-content">
-                  <h6
-                    className="hero-sub-title"
-                    data-animation="fadeInUp"
-                    data-delay=".25s"
-                  >
-                    <i className="far fa-house-chimney-heart"></i> Best Elderly
-                    Care
-                  </h6>
-                  <h1
-                    className="hero-title"
-                    data-animation="fadeInRight"
-                    data-delay=".50s"
-                  >
-                    Live in care & <span>your family</span> will love
-                  </h1>
-                  <p data-animation="fadeInLeft" data-delay=".75s">
-                    There are many variations of passages orem psum available
-                    but the majority have suffered alteration in some form by
-                    injected humour.
-                  </p>
-                  <div
-                    className="hero-btn"
-                    data-animation="fadeInUp"
-                    data-delay="1s"
-                  >
-                    <Link href="/about" className="theme-btn">
-                      About More<i className="fas fa-arrow-right"></i>
-                    </Link>
-                    <Link href="/contact" className="theme-btn2">
-                      Learn More<i className="fas fa-arrow-right"></i>
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
